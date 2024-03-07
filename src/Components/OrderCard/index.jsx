d80f0d03d9ba061dc38a89af13deadf3d1e59548
josemsantosv <josemsantosv@gmail.com>
2024-03-07 12:12:46 -0500

    eliminar productos del carrito

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,5 +1,5 @@
 const OrderCard = props => {
-    const { title, imageUrl, price } = props
+    const { id, title, imageUrl, price, handleDelete } = props
 
     return (
         <div className="flex justify-between items-center mb-3">
@@ -11,7 +11,7 @@ const OrderCard = props => {
         </div>
         <div className='flex items-center gap-2'>
           <p className='text-lg font-medium'>{price}</p>
-          <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+          <svg  onClick={() => handleDelete(id)} xmlns="https://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 cursor-pointer text-black">
           <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
         </svg>
         </div>
